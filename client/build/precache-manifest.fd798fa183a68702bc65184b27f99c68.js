2cf054cd06eb216a843524e79065c0e0438063d7
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-25 11:34:23 -0400

    Add React build for Render deployment

---
+++ client/build/precache-manifest.fd798fa183a68702bc65184b27f99c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34ddd11ffff9ae5cf0b7",
+    "url": "/static/css/main.2b8a4582.chunk.css"
+  },
+  {
+    "revision": "34ddd11ffff9ae5cf0b7",
+    "url": "/static/js/main.34ddd11f.chunk.js"
+  },
+  {
+    "revision": "90c86b65ce63dabe9802",
+    "url": "/static/js/1.90c86b65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4021382e60ccd917e4fef3ad46dc1511",
+    "url": "/index.html"
+  }
+];
